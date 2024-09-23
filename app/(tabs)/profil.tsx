5e7909c13bdc059af40027fae902fa368947225f
David <d.almeida@pilboard.fr>
2024-09-23 13:44:05 +0200

    init project

---
+++ app/(tabs)/profil.tsx
@@ -0,0 +1,12 @@
+import {ThemedView} from "@/components/ThemedView";
+import {ThemedText} from "@/components/ThemedText";
+
+export default function Profil(){
+    return (
+        <ThemedView style={{flex: 1}}>
+            <ThemedText>Profil</ThemedText>
+        </ThemedView>
+
+    )
+
+}
