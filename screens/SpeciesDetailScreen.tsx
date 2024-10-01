2d8eabb06b98ae382ea1fdfb9ec1f04ccbcc2436
yoanbrugiere <yoan.brugiere@etu.uca.fr>
2024-10-01 11:49:33 +0200

    Début SpeciesDetailScreen

---
+++ screens/SpeciesDetailScreen.tsx
@@ -0,0 +1,22 @@
+
+import {FlatList, StyleSheet} from "react-native";
+import {SafeView} from "@/components/ui/SafeView";
+import Capture from "@/model/Capture";
+
+interface SpeciesDetailScreenProps {
+    captures: Capture[]
+}
+
+export default function SpeciesDetailScreen(props: SpeciesDetailScreenProps) {
+
+    return (
+        <SafeView>
+
+
+        </SafeView>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+});
