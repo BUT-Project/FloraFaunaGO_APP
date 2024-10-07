2baa0854691d50d054a75983550d42000b4b363d
patrick.brugiere <patrick.BRUGIERE@etu.uca.fr>
2024-10-07 15:22:12 +0200

    ajout data

---
+++ dal/StubLib/StubData.ts
@@ -0,0 +1,8 @@
+import {CaptureList, SpecieList, UserList} from "./Data"
+
+export default class StubData {
+    private ListSpecie = SpecieList
+    private ListUser = UserList
+    private ListCapture = CaptureList
+
+}
