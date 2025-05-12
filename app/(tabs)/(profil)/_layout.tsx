0052b9cbcdb4e2846759de373272b317d1f5553f
patrick.brugiere <patrick.BRUGIERE@etu.uca.fr>
2025-05-12 16:16:43 +0200

    fix2

--- app/(tabs)/(profil)/_layout.tsx
+++ app/(tabs)/(profil)/_layout.tsx
@@ -19,7 +19,7 @@ export default function Layout() {
             options={{
 
                 animation:'slide_from_bottom',
-                //presentation: 'modal',
+                presentation: 'modal',
                 headerBackVisible: false,
                 headerLeft : () =>(
                     <Link href="/(profil)/profil" asChild>
