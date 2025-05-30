9d2def5d3609cc258e74581b7b6c47f1323b8b10
Yoan BRUGIÈRE <yoan.brugiere@etu.uca.fr>
2025-05-30 17:00:19 +0200

    nouvelle tentative, enfaite il s'emble que c'est impossible de récup la permission avec expo go

--- hooks/usePodometer.tsx
+++ hooks/usePodometer.tsx
@@ -10,59 +10,35 @@ export function usePodometer({isAuthenticated=false}:usePodometerProps){
     const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
     const updateStepCount = useAuthStore((state) => state.updateStepCount);
 
-    const subscribe = async () => {
-        
-        const isAvailable = await Pedometer.isAvailableAsync();
-        setIsPedometerAvailable(String(isAvailable));
-
-        //Possibilité d'afficher le nombre de pas dans les dernières 24h (x Heures)
-        // if (isAvailable) {
-        // const end = new Date();
-        // const start = new Date();
-        // start.set
-        // Date(end.getDate() - 1);
-
-        // const pastStepCountResult = await Pedometer.getStepCountAsync(start, end);
-        // if (pastStepCountResult) {
-        //     setPastStepCount(pastStepCountResult.steps);
-        // }
-
-        return Pedometer.watchStepCount(result => {
-            console.log("result :",result)
-            updateStepCount(result.steps);
-        });
-    }
-
     useEffect(() => {
-        if(!isAuthenticated) return;
+        if (!isAuthenticated) return;
 
-        const askPermissions = async() => {
-            const result =    await Pedometer.requestPermissionsAsync();
-            console.log(result)
-            return result
-        }
-        askPermissions();
-
-        const permissions = async() =>{
-            const permissions =  await Pedometer.getPermissionsAsync();
-            console.log(permissions)
-            return permissions;
-        } 
-        
-        permissions()
         let subscription: { remove: () => void } | null = null;
 
         const start = async () => {
-            subscription = await subscribe();
+            const result = await Pedometer.requestPermissionsAsync();
+            console.log('Permission result:', result);
+
+            if (result.granted) {
+            const isAvailable = await Pedometer.isAvailableAsync();
+            setIsPedometerAvailable(String(isAvailable));
+
+            subscription = Pedometer.watchStepCount(result => {
+                console.log("result :", result);
+                updateStepCount(result.steps);
+            });
+            } else {
+            console.warn("Permission denied. Invite user to enable it manually.");
+            }
         };
 
         start();
-        console.log(subscription)
+
         return () => {
             if (subscription) {
-                subscription.remove();
+            subscription.remove();
             }
-    };
+        };
     }, [isAuthenticated]);
 
     return {
