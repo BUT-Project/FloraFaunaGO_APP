6d686b01a35325b146f2ce0d011285a91089a8c8
David <David.D_ALMEIDA@etu.uca.fr>
2025-06-14 23:04:52 +0200

    REFAC : Reorganize imports and clean up formatting in ProfilScreen component

--- screens/ProfilScreen.tsx
+++ screens/ProfilScreen.tsx
@@ -1,55 +1,55 @@
 import {Dimensions, FlatList, Image, StyleSheet, TouchableOpacity, useColorScheme} from "react-native";
 import React, {useEffect, useState} from "react";
 import SucessListItemVertical from "@/components/SucessListItemVertical";
-import {ThemedView,ThemedText,ThemedIcon} from "@/components/ui/themed";
+import {ThemedIcon, ThemedText, ThemedView} from "@/components/ui/themed";
 import {Link} from 'expo-router';
 import {AntDesign, FontAwesome5, FontAwesome6, Ionicons} from "@expo/vector-icons";
 import {Success} from "@/model/domain/Success";
 import {PagedRequest} from "@/shared/PagedRequest";
 import {useAuthStore} from "@/context/zustand/store/useAuthStore";
-import { SafeView } from "@/components/ui/SafeView";
-import { Colors } from "@/constants/Colors";
+import {SafeView} from "@/components/ui/SafeView";
+import {Colors} from "@/constants/Colors";
 import * as ImagePicker from 'expo-image-picker';
-import { useUserStore } from "@/context/zustand/store/useUserStore";
-import { SuccessManager } from "@/dal/manager/SuccessManager";
+import {useUserStore} from "@/context/zustand/store/useUserStore";
+import {SuccessManager} from "@/dal/manager/SuccessManager";
 import StubData from "@/dal/StubLib/StubData";
 
 let ProfileImage: {};
 ProfileImage = require("../assets/images/ProfileImage.jpeg");
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 export default function ProfilScreen() {
     const [Successes, setSuccesses] = useState<Success[]>([]);
     const [page, setPage] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
     const [totalPages, setTotalPages] = useState(1);
-    const colorScheme =  useColorScheme() ?? 'light';
+    const colorScheme = useColorScheme() ?? 'light';
     const theme = Colors[colorScheme];
-    const [stepCount,setStepCount] = useState(1)
+    const [stepCount, setStepCount] = useState(1)
     const [imageUri, setImageUri] = useState<string | null | undefined>(null);
     const user = useAuthStore((state) => state.user);
     //modifier car les captures plus dans le modele (david)
     const species = useAuthStore((state) => new Set(state.user?.captures.map(c => c.specie.id)).size);
     const family = useAuthStore((state) => new Set(state.user?.captures.map(c => c.specie.family)).size);
     const dataUser = useUserStore();
-    const sucessManager =  new SuccessManager(StubData.getInstance().successRepository!, StubData.getInstance().successStateRepository);
+    const sucessManager = new SuccessManager(StubData.getInstance().successRepository!, StubData.getInstance().successStateRepository);
 
     const fetchSuccesses = async (currentPage: number) => {
         setIsLoading(true);
         try {
-            const PageRequest : PagedRequest = {
+            const PageRequest: PagedRequest = {
                 index: currentPage,
                 count: 9
             }
-    const pagedSuccesses = await sucessManager.getAll(PageRequest);
-    const successes = pagedSuccesses.items
-    console.log("Successes", successes);
-    setSuccesses(successes);
+            const pagedSuccesses = await sucessManager.getAll(PageRequest);
+            const successes = pagedSuccesses.items
+            console.log("Successes", successes);
+            setSuccesses(successes);
 
-    const total = pagedSuccesses?.total ?? 0;
+            const total = pagedSuccesses?.total ?? 0;
 
-    setTotalPages(Math.ceil(total / 9));
-    
-    } catch (error) {
+            setTotalPages(Math.ceil(total / 9));
+
+        } catch (error) {
             console.error('Erreur lors de la récupération des succès :', error);
         } finally {
             setIsLoading(false);
@@ -62,56 +62,57 @@ export default function ProfilScreen() {
 
     const pickImage = async () => {
         const result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes:ImagePicker.MediaTypeOptions.Images,
-          quality: 1,
-          base64: true,
-          allowsEditing: true,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+            base64: true,
+            allowsEditing: true,
         });
-    
+
         if (!result.canceled) {
-          setImageUri(result.assets[0].uri);
-          if(user && result.assets[0].base64) {
-          user.image = result.assets[0].base64
-          dataUser.updateUser(user.id,user)
-          }
+            setImageUri(result.assets[0].uri);
+            if (user && result.assets[0].base64) {
+                user.image = result.assets[0].base64
+                dataUser.updateUser(user.id, user)
+            }
         }
-      };
+    };
     const renderHeader = () => (
         <ThemedView>
-            <Link href={"/(profil)/settings"}  style={{ alignSelf: "flex-end",}} asChild>
+            <Link href={"/(profil)/settings"} style={{alignSelf: "flex-end",}} asChild>
                 <TouchableOpacity>
                     <ThemedIcon size={30} name="settings" style={styles.settings}/>
                 </TouchableOpacity>
             </Link>
             <TouchableOpacity style={styles.imageContainer} onPress={pickImage}>
-        <Image
-          source={
-            imageUri
-              ? { uri: imageUri }
-              : require('./../assets/images/ProfileImage.jpeg')
-          }
-          style={styles.image}
-        />
-      </TouchableOpacity>
+                <Image
+                    source={
+                        imageUri
+                            ? {uri: imageUri}
+                            : require('./../assets/images/ProfileImage.jpeg')
+                    }
+                    style={styles.image}
+                />
+            </TouchableOpacity>
             <ThemedView style={styles.userInfoContainer}>
                 <ThemedText style={styles.username}>{user?.username || 'Username not available'}
-                <Link href={"/(profil)/edit_profile"} asChild>
-            <TouchableOpacity>
-            <Ionicons name="pencil" color={theme.text} size={20} />
-            </TouchableOpacity>
-            </Link></ThemedText>
+                    <Link href={"/(profil)/edit_profile"} asChild>
+                        <TouchableOpacity>
+                            <Ionicons name="pencil" color={theme.text} size={20}/>
+                        </TouchableOpacity>
+                    </Link></ThemedText>
                 <ThemedText style={styles.email}>{user?.email || 'Email not available'}</ThemedText>
-                
+
             </ThemedView>
 
             <ThemedView style={styles.lineContainer}>
-                <ThemedView style={styles.line} />
-                <ThemedText type={"title"} style={styles.title} >Statistiques</ThemedText><ThemedView style={styles.line} />
+                <ThemedView style={styles.line}/>
+                <ThemedText type={"title"} style={styles.title}>Statistiques</ThemedText><ThemedView
+                style={styles.line}/>
             </ThemedView>
 
             <ThemedView style={styles.container}>
                 <FontAwesome5 size={32} name="walking" style={[styles.settings, {color: theme.text}]}/>
-                <ThemedText style={styles.text}>  Distance marchées {stepCount} </ThemedText>
+                <ThemedText style={styles.text}> Distance marchées {stepCount} </ThemedText>
             </ThemedView>
             <ThemedView style={styles.container}>
                 <FontAwesome6 size={30} name="circle-question" style={[styles.settings, {color: theme.text}]}/>
@@ -122,26 +123,26 @@ export default function ProfilScreen() {
                 <ThemedText style={styles.text}> Familles complétées : {family}</ThemedText>
             </ThemedView>
             <ThemedView style={styles.container}>
-                <AntDesign size={30} name="clockcircleo" style={[styles.settings, {color:theme.text}]}/>
+                <AntDesign size={30} name="clockcircleo" style={[styles.settings, {color: theme.text}]}/>
                 <ThemedText style={styles.text}>Date d'inscription : {user?.inscriptionDate &&
-                        new Date(user.inscriptionDate).toLocaleString('fr-FR', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric',
-                        })}</ThemedText>
+                    new Date(user.inscriptionDate).toLocaleString('fr-FR', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                    })}</ThemedText>
             </ThemedView>
             <ThemedView style={styles.lineContainer}>
-                <ThemedView style={styles.line} />
-                <ThemedText type={"title"} style={styles.title} >Succès</ThemedText><ThemedView style={styles.line} />
+                <ThemedView style={styles.line}/>
+                <ThemedText type={"title"} style={styles.title}>Succès</ThemedText><ThemedView style={styles.line}/>
             </ThemedView>
             <ThemedView style={styles.pagination}>
                 <TouchableOpacity
                     onPress={() => setPage((prev) => Math.max(prev - 1, 0))}
                     disabled={page === 1}
                 >
-                    <ThemedIcon 
+                    <ThemedIcon
                         name="chevron-back-circle"
-                        color={page === 1? theme.successBackground : theme.tint}
+                        color={page === 1 ? theme.successBackground : theme.tint}
                         size={40}
                     />
                 </TouchableOpacity>
@@ -150,8 +151,8 @@ export default function ProfilScreen() {
                     onPress={() => setPage((prev) => Math.min(prev + 1, totalPages))}
                     disabled={page === totalPages}
                 >
-                    <ThemedIcon 
-                        name="chevron-forward-circle" 
+                    <ThemedIcon
+                        name="chevron-forward-circle"
                         color={page === totalPages ? theme.successBackground : theme.tint}
                         size={40}
                     />
@@ -165,7 +166,7 @@ export default function ProfilScreen() {
         <SafeView disableBottomInset>
             <FlatList
                 style={styles.list}
-                data={Successes?? []}
+                data={Successes ?? []}
                 keyExtractor={(item) => item.nom}
                 renderItem={({item}) => <SucessListItemVertical items={item}/>}
                 numColumns={3}
@@ -177,7 +178,7 @@ export default function ProfilScreen() {
 }
 
 const styles = StyleSheet.create({
-    imageContainer:{
+    imageContainer: {
         alignItems: 'center',
         marginTop: 50,
     },
@@ -185,8 +186,8 @@ const styles = StyleSheet.create({
         width: 150,
         height: 150,
         borderRadius: 75,
-      },
-    listWrapper:{
+    },
+    listWrapper: {
         justifyContent: "center",
         marginBottom: 10
     },
@@ -202,7 +203,7 @@ const styles = StyleSheet.create({
     },
     list: {
         width: '100%',
-        alignSelf : "center",
+        alignSelf: "center",
     },
     pagination: {
         flexDirection: 'row',
@@ -219,28 +220,28 @@ const styles = StyleSheet.create({
         marginVertical: 20,
     },
     container: {
-        display:"flex",
+        display: "flex",
         flexDirection: 'row',
         alignItems: 'center',
-        alignSelf : 'center',
+        alignSelf: 'center',
         justifyContent: 'flex-start',
         height: 50,
         marginVertical: 5,
     },
-    title : {
-        margin:20,
+    title: {
+        margin: 20,
         textAlign: "center"
     },
     text: {
-        textAlignVertical:'center',
+        textAlignVertical: 'center',
     },
     settings: {
         marginRight: 10,
     },
     profile: {
         alignSelf: "center",
-        width: width* 0.4,
-        height: width* 0.4,
+        width: width * 0.4,
+        height: width * 0.4,
         margin: "10%",
         borderRadius: 500,
     },
@@ -257,7 +258,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#666',
     },
-    button:{
-        borderRadius:30,
+    button: {
+        borderRadius: 30,
     }
 });
